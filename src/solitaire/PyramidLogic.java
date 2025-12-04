701b70bbf648ba710e9eff9fc930c41d02959fc9
Mason Simmons <solfsimmons@gmail.com>
2025-12-03 23:55:02 -0500

    got PyramidGame to open from the Menu after selecting difficulty

--- src/solitaire/PyramidLogic.java
+++ src/solitaire/PyramidLogic.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
-public class PyramidLogic extends Solitaire {
+public class PyramidLogic {
 	private static final long serialVersionUID = 1L;
 	Deck stock;
     ArrayList<Pile> stockAndWaste;
@@ -13,21 +13,23 @@ public class PyramidLogic extends Solitaire {
     Pile wastePile; //reference variable so we don't have to keep calling stockAndWaste.get(1)
     List<CardNode> pyramidCards; // the pyramid cards!
     int difficulty; //0 for infinite stockFlips, 1 for 2 stockFlips
-    int stockFlips;
+    int restocks;
     private static final char[] ranks = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K' };
     Stack<PyramidMove> pastMoves;
 
 	public PyramidLogic(int diff){
         difficulty = diff;  //Endless flips or no?
-        stockFlips = 0;     //stockFlips is initialized;
-        stock = new Deck(); //The deck is initialized.
+        restocks = 0;     //stockFlips is initialized;
+        stock = new Deck(1); //The deck is initialized.
 		stock.shuffle();
+        pyramidCards = new ArrayList<>();
         for(int i = 0; i < 28; ++i) //The pyramid is initialized.
         {
             pyramidCards.add(new CardNode(stock.pop()));
         }
 
-        stockAndWaste.add(new Pile(2));//The stock and waste piles are initialized and their aliases are set.
+        stockAndWaste = new ArrayList<>();//The stock and waste piles are initialized and their aliases are set.
+        stockAndWaste.add(new Pile(2));
         stockAndWaste.add(new Pile(2));
         stockPile = stockAndWaste.get(0);
         wastePile = stockAndWaste.get(1);
@@ -46,13 +48,13 @@ public class PyramidLogic extends Solitaire {
     {
         if(stockPile.isEmpty())
         {
-            if(difficulty == 1 && stockFlips >= 2)//if the player used up their flips, they can't anymore
+            if(difficulty == 1 && restocks >= 2)//if the player used up their flips, they can't anymore
                 return;
 
             Collections.reverse(wastePile);
             stockPile.addAll(wastePile);
             wastePile.clear();
-            stockFlips++;
+            restocks++;
             pastMoves.push(new PyramidMove(2));
         }
         else {
@@ -97,8 +99,7 @@ public class PyramidLogic extends Solitaire {
         return pyramidCards.getFirst().isRemoved();
     }
 
-    @Override
-    protected void undoLastMove() {
+    protected void undoMove() {
         if(pastMoves.isEmpty())
             return;
 
@@ -117,31 +118,11 @@ public class PyramidLogic extends Solitaire {
             Collections.reverse(stockPile);
             wastePile.addAll(stockPile);
             stockPile.clear();
-            stockFlips--;
+            restocks--;
         }
         else {
             lastMove.firstCard.setRemoved(false);
             lastMove.secondCard.setRemoved(false);
         }
-    }
-
-    @Override
-    protected void newGame() {
-
-    }
-
-    @Override
-    void saveGame() {
-
-    }
-
-    @Override
-    public GameSave makeSave() {
-        return null;
-    }
-
-    @Override
-    public void loadSave(PileSave save) {
-
     }
 }
